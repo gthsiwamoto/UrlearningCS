c8d369c136560f7eab70665393945c51bdef0b25
Shunsuke Iwamoto <gm.siwamoto@gmail.com>
2015-11-09 17:48:20 +0900

    クラスAdTreeScoringを追加

---
+++ AdTreeScoring/AdTreeScoring.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Datastructures;
+
+namespace AdTreeScoring
+{
+    class AdTreeScoring
+    {
+        public AdTreeScoring() { }
+
+        public void Execute(string[] args)
+        {
+            // 引数のチェック
+            if (args.Length != 2)
+            {
+                // エラーメッセージ表示
+                Console.WriteLine("[エラー] 引数の数が不正です");
+                Environment.Exit(0);
+            }
+
+            // オプションのチェック
+            // 暫定的に初期値を代入
+            int rMin = 5; // The minimum number of records in the AD-tree nodes.
+            char delimiter = ',';
+            bool hasHeader = true;
+            string sf = "BIC";
+            int maxParents = 0;
+
+            // csvファイルの読み込み
+            RecordFile recordFile = new RecordFile();
+            recordFile.ReadRecord(args[0], hasHeader, delimiter);
+            //recordFile.Print();
+
+            // BayesianNetworkの初期化
+            BayesianNetwork network = new BayesianNetwork(recordFile);
+
+            // AD-Treeの初期化
+            AdTree adTree = new AdTree(rMin, network, recordFile);
+
+            // scoring functionの設定
+            sf = sf.ToLower();
+            if (maxParents > network.Size() || maxParents < 1)
+            {
+                maxParents = network.Size() - 1;
+            }
+
+            if (sf == "bic")
+            {
+                int maxParentCount = (int)Math.Log(2 * recordFile.Size() / Math.Log(recordFile.Size()));
+                if(maxParentCount < maxParents)
+                {
+                    maxParents = maxParentCount;
+                }
+            }
+        }
+    }
+}
--- AdTreeScoring/Program.cs
+++ AdTreeScoring/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Datastructures;
+using AdTreeScoring;
 
 namespace AdTreeScoring
 {
@@ -7,36 +7,8 @@ namespace AdTreeScoring
     {
         static void Main(string[] args)
         {
-            // 引数のチェック
-            if (args.Length != 2)
-            {
-                // エラーメッセージ表示
-                Console.WriteLine("[エラー] 引数の数が不正です");
-                Environment.Exit(0);
-            }
-
-            // オプションのチェック
-            // 暫定的に初期値を代入
-            int rMin = 5; // The minimum number of records in the AD-tree nodes.
-            char delimiter = ',';
-            bool hasHeader = true;
-            string sf = "BIC";
-            int maxParents = 0;
-
-            // csvファイルの読み込み
-            RecordFile recordFile = new RecordFile();
-            recordFile.ReadRecord(args[0], hasHeader, delimiter);
-            //recordFile.Print();
-
-            // BayesianNetworkの初期化
-            BayesianNetwork network = new BayesianNetwork(recordFile);
-
-            // AD-Treeの初期化
-            AdTree adTree = new AdTree(rMin, network, recordFile);
-
-            // scoring functionの設定
-            sf = sf.ToLower();
-
+            AdTreeScoring ats = new AdTreeScoring();
+            ats.Execute(args);
             //Console.In.ReadLine();
         }
     }
